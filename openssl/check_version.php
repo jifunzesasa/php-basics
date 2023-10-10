84123124908c95ec7e4794330551117967cee72e
Alpha Olomi <alphaolomi@gmail.com>
2023-10-10 08:39:33 +0300

    Create check_version.php

---
+++ openssl/check_version.php
@@ -0,0 +1,12 @@
+<?php
+
+// https://www.php.net/manual/en/openssl.constversion.php
+
+// OpenSSL
+echo "openssl version text: " . OPENSSL_VERSION_TEXT . "\n";
+echo "openssl version number:  " . OPENSSL_VERSION_NUMBER . "\n";
+
+
+// PHP
+echo "PHP_VERSION: " . PHP_VERSION . "\n";
+echo "PHP_MAJOR_VERSION: " . PHP_MAJOR_VERSION . "\n";
