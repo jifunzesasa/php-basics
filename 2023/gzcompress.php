285d31ff3b0fb7a257eeef4b7168ba45cbc6024d
Alpha Olomi <alphaolomi@gmail.com>
2023-12-21 08:38:44 +0300

    Update gzcompress.php

--- 2023/gzcompress.php
+++ 2023/gzcompress.php
@@ -16,10 +16,16 @@ And miles to go before I sleep.
 POEM;
 
 
-
+// The level of compression. Can be given as 0 for no compression up to 9 for maximum compression.
+// If -1 is used, the default compression of the zlib library is used which is 6.
 $level = MAXIMUM_COMPRESSION_LEVEL;
 
 $compressed = gzcompress($poemText, $level);
 
 
-echo $compressed;
+if ($compressed === false) {
+    echo "The compression failed.";
+} else {
+    echo "The compression succeeded.";
+    echo $compressed;
+}
